f3ce52f9f1ce906c626b513c429ed69c65d26b80
Aman Agarwal <aman@appsmith.com>
2023-12-23 23:44:31 +0530

    feat: updated next config js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
+// const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-let assetPrefix = "";
-let basePath = "/";
+// let assetPrefix = "";
+// let basePath = "/";
 
-if (isGithubActions) {
-	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+// if (isGithubActions) {
+// 	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
-	assetPrefix = `/${repo}/`;
-	basePath = `/${repo}`;
-}
+// 	assetPrefix = `/${repo}/`;
+// 	basePath = `/${repo}`;
+// }
 
 module.exports = {
-	assetPrefix: assetPrefix,
-	basePath: basePath,
+	// assetPrefix: assetPrefix,
+	// basePath: basePath,
 	output: "export",
 };
